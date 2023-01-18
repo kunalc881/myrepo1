6d3a313d74364e6c828daec9d55cc87d10babd23
kunalc881 <122966979+kunalc881@users.noreply.github.com>
2023-01-18 13:48:37 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,10 +2,10 @@
 <html>
 <body>
 
-<h1>My first PHP page using openshift S2I</h1>
+<h1>Welcome to my first PHP page using openshift S2I</h1>
 
 <?php
-echo "Welcome to my webpage it is updated now.!";
+echo "this is old version of webpage...!";
 ?>
 
 </body>
