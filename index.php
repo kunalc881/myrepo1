74e55f74b592eecbd25a06919d81388ad316c6a0
kunalc881 <122966979+kunalc881@users.noreply.github.com>
2023-01-30 11:19:30 +0530

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 
 <?php
 echo "this is my first web page using S2I webhooks....!";
-echo "webhooks updated the result when we update the code in github...!";
+echo "webhooks updated recently before 2 min...!";
 ?>
 
 </body>
