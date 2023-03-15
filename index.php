501804245713f10479e82aae1c4ebb912638d989
kunalc881 <122966979+kunalc881@users.noreply.github.com>
2023-03-15 20:00:22 +0530

    Update index.php

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
 <?php
 echo "this is my first web page using S2I....!";
 echo "welcome to my webpage hello from kunal thank you ....!";
+echo "this is successfully deployed on production server"
 ?>
 
 </body>
